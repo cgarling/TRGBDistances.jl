49161313932b3ca938eafa89e0b5e80641e73b7b
Documenter.jl <documenter@juliadocs.github.io>
2025-09-01 14:16:02 +0000

    build based on cc0b1a4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TRGBDistances","page":"Home","title":"TRGBDistances","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TRGBDistances.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
