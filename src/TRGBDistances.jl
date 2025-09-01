02284e135182a2c86fc9da865f2857631fa13951
cgarling <chris.t.garling@gmail.com>
2025-09-01 15:36:08 -0400

    Try other integration types

--- src/TRGBDistances.jl
+++ src/TRGBDistances.jl
@@ -2,12 +2,16 @@ module TRGBDistances
 
 using Distributions: pdf, logpdf, Normal
 using QuadGK: quadgk
+using Trapz: trapz
+using HCubature: hcubature
 using Interpolations: linear_interpolation
 
 include("calibrations.jl")
 
 abstract type LuminosityFunction{T} end
 (l::LuminosityFunction)(m) = ψ(l, m)
+Base.Broadcast.broadcastable(t::LuminosityFunction) = Ref(t)
+
 
 struct BrokenPowerLaw{T} <: LuminosityFunction{T}
     m_trgb::T
@@ -18,15 +22,28 @@ end
 BrokenPowerLaw(m_trgb, a, b, c) = BrokenPowerLaw(promote(m_trgb, a, b, c)...)
 ψ(b::BrokenPowerLaw, m) = m >= b.m_trgb ? exp10(b.a * (m - b.m_trgb) + b.b) : exp10(b.c * (m - b.m_trgb))
 
-# Computes Equation 4 of Makarov 2006
+# Computing Equation 4 of Makarov 2006
+@inline function ϕ_integrand(x, m, b::LuminosityFunction, err_func, complete_func, bias_func)
+    return ψ(b, x) * pdf(Normal(x + bias_func(x), err_func(x)), m) * complete_func(x)
+end
 function ϕ(m, b::LuminosityFunction, err_func, complete_func, bias_func)
-    # σ = err_func(m)
     # # Defining bias_func(m) = <measured> - <input>, so \bar{m}(m^\prime) = m^\prime + bias_func(m)
-    # mp = m + bias_func(m)
-    # return quadgk(x -> ψ(b, m) * pdf(Normal(mp, σ), m) * complete_func(m), mp - 10σ, mp + 10σ)[1]
-    # Defining bias_func(m) = <measured> - <input>, so \bar{m}(m^\prime) = m^\prime + bias_func(m)
-    return quadgk(x -> ψ(b, x) * pdf(Normal(x + bias_func(x), err_func(x)), m) * complete_func(x), m - 1, m + 1)[1]
-    # return quadgk(x -> ψ(b, x) * pdf(Normal(m, err_func(x)), x + bias_func(x)) * complete_func(x), m - 1, m + 1)[1]
+    # x = range(m-1, m+1; length=1000)
+    # return trapz(x, ϕ_integrand.(x, m, b, Ref(err_func), Ref(complete_func), Ref(bias_func)))
+    return quadgk(x -> ϕ_integrand(x, m, b, err_func, complete_func, bias_func), m - 1, m + 1)[1]
+end
+# Double integral that appears in Makarov 2006 Equation 7; transform variables to rectangular integration region
+function transformed_ϕ_integrand(uv, b::LuminosityFunction, err_func, complete_func, bias_func)
+    u, v = uv
+    x = v + 2u - 1 # 2v + u - 1
+    return 2 * ϕ_integrand(x, v, b, err_func, complete_func, bias_func)
+end
+function ϕ2(limits, b::LuminosityFunction, err_func, complete_func, bias_func)
+    return quadgk(x -> ϕ(x, b, err_func, complete_func, bias_func), limits...)[1]
+    # return hcubature(uv -> transformed_ϕ_integrand(uv, b, err_func, complete_func, bias_func), [0.0, limits[1]], [1.0, limits[2]])[1]
+    # x = 0.0:0.001:1.0
+    # y = range(limits...; length=1000)
+    # return trapz((x,y), [transformed_ϕ_integrand([i,j], b, err_func, complete_func, bias_func) for i=x,j=y])
 end
 
 struct LuminosityFunctionData{T,A,B,C,D}
@@ -45,7 +62,10 @@ function (fit::LuminosityFunctionData)(θ)
         return Inf
     end
     model = fit.model(θ...)
-    return -sum(log(ϕ(m, model, fit.err_func, fit.complete_func, fit.bias_func)) for m in fit.mags) + length(fit.mags) * log(quadgk(x -> ϕ(x, model, fit.err_func, fit.complete_func, fit.bias_func), extrema(fit.mags)...)[1])
+    # return -sum(log(ϕ(m, model, fit.err_func, fit.complete_func, fit.bias_func)) for m in fit.mags) + length(fit.mags) * log(quadgk(x -> ϕ(x, model, fit.err_func, fit.complete_func, fit.bias_func), extrema(fit.mags)...)[1])
+    # return quadgk(x -> ϕ(x, model, fit.err_func, fit.complete_func, fit.bias_func), extrema(fit.mags)...)[1]
+    # return ϕ2(extrema(fit.mags), model, fit.err_func, fit.complete_func, fit.bias_func)
+    return -sum(log(ϕ(m, model, fit.err_func, fit.complete_func, fit.bias_func)) for m in fit.mags) + length(fit.mags) * log(ϕ2(extrema(fit.mags), model, fit.err_func, fit.complete_func, fit.bias_func))
 end
 
 
